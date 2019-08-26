44a235a804736246403cb15d3e79ca7e8cdc07cf
WilliamConnatser <WilliamConnatser@users.noreply.github.com>
2019-08-26 14:06:11 -0500

    made endpoints

--- index.js
+++ index.js
@@ -17,7 +17,37 @@ server.post('/add-room', (req,res) => {
     })
     .catch((error) => {
         res.status(500).send({message: error.message})
+    });
+});
+
+server.put('/update-room', (req,res) => {
+    db.updateRoom(req.body)
+    .then((result) => {
+        res.status(200).send(result);
     })
-})
+    .catch((error) => {
+        res.status(500).send({message: error.message})
+    });
+});
+
+server.get('/get-room/:id', (req,res) => {
+    db.getRoom(req.params.id)
+    .then((result) => {
+        res.status(200).send(result);
+    })
+    .catch((error) => {
+        res.status(500).send({message: error.message})
+    });
+});
+
+server.get('/get-graph', (req,res) => {
+    db.getGraph()
+    .then((result) => {
+        res.status(200).send(result);
+    })
+    .catch((error) => {
+        res.status(500).send({message: error.message})
+    });
+});
 
 server.listen(port, () => console.log(`\u{1F680}\u{1F680}\u{1F680} http://localhost:${port}/ \u{1F680}\u{1F680}\u{1F680}`));
